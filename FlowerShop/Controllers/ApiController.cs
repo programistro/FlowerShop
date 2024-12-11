7cddf9165cc7fbfc1f5e85a3b9c87634ab0f2f19
prrogramistro <r.grigorev1502@gmail.com>
2024-12-11 21:04:02 +0300

    ..

---
+++ FlowerShop/Controllers/ApiController.cs
@@ -0,0 +1,123 @@
+﻿using ClosedXML.Excel;
+using FlowerShop.Core;
+using FlowerShop.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerShop.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ApiController : ControllerBase
+{
+    private readonly ILogger<ApiController> _logger;
+    
+    private readonly AppDbContext _context;
+
+    public ApiController(ILogger<ApiController> logger, AppDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+    
+    [HttpGet("export-flowers")]
+    public IActionResult ExportFlowersToExcel()
+    {
+        var list = _context.Flowers.ToList();
+
+        using (XLWorkbook workbook = new XLWorkbook())
+        {
+            var worksheet = workbook.Worksheets.Add("Цветы");
+
+            worksheet.Cell("A1").Value = "Имя";
+            worksheet.Cell("B1").Value = "Цена";
+            worksheet.Row(1).Style.Font.Bold = true;
+
+            //нумерация строк/столбцов начинается с индекса 1 (не 0)
+            for (int i = 0; i < list.Count; i++)
+            {
+                worksheet.Cell(i + 2, 1).Value = list[i].Name;
+                worksheet.Cell(i + 2, 2).Value = list[i].Price; // Исправлено на столбец B
+            }
+
+            using (var stream = new MemoryStream())
+            {
+                workbook.SaveAs(stream);
+                stream.Position = 0;
+
+                return File(
+                    fileContents: stream.ToArray(),
+                    contentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    fileDownloadName: $"flowers_{DateTime.UtcNow.ToString("yyyyMMdd_HHmmss")}.xlsx"
+                );
+            }
+        }
+    }
+    
+    [HttpGet("export-toys")]
+    public IActionResult ExportToysToExcel()
+    {
+        var list = _context.Toys.ToList();
+
+        using (XLWorkbook workbook = new XLWorkbook())
+        {
+            var worksheet = workbook.Worksheets.Add("Игрушки");
+
+            worksheet.Cell("A1").Value = "Имя";
+            worksheet.Cell("B1").Value = "Цена";
+            worksheet.Row(1).Style.Font.Bold = true;
+
+            //нумерация строк/столбцов начинается с индекса 1 (не 0)
+            for (int i = 0; i < list.Count; i++)
+            {
+                worksheet.Cell(i + 2, 1).Value = list[i].Name;
+                worksheet.Cell(i + 2, 2).Value = list[i].Price; // Исправлено на столбец B
+            }
+
+            using (var stream = new MemoryStream())
+            {
+                workbook.SaveAs(stream);
+                stream.Position = 0;
+
+                return File(
+                    fileContents: stream.ToArray(),
+                    contentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    fileDownloadName: $"flowers_{DateTime.UtcNow.ToString("yyyyMMdd_HHmmss")}.xlsx"
+                );
+            }
+        }
+    }
+    
+    [HttpGet("export-packs")]
+    public IActionResult ExportPacksToExcel()
+    {
+        var list = _context.Packs.ToList();
+
+        using (XLWorkbook workbook = new XLWorkbook())
+        {
+            var worksheet = workbook.Worksheets.Add("Упаковки");
+
+            worksheet.Cell("A1").Value = "Имя";
+            worksheet.Cell("B1").Value = "Цена";
+            worksheet.Row(1).Style.Font.Bold = true;
+
+            //нумерация строк/столбцов начинается с индекса 1 (не 0)
+            for (int i = 0; i < list.Count; i++)
+            {
+                worksheet.Cell(i + 2, 1).Value = list[i].Name;
+                worksheet.Cell(i + 2, 2).Value = list[i].Price; // Исправлено на столбец B
+            }
+
+            using (var stream = new MemoryStream())
+            {
+                workbook.SaveAs(stream);
+                stream.Position = 0;
+
+                return File(
+                    fileContents: stream.ToArray(),
+                    contentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    fileDownloadName: $"flowers_{DateTime.UtcNow.ToString("yyyyMMdd_HHmmss")}.xlsx"
+                );
+            }
+        }
+    }
+}
---
+++ FlowerShop/Migrations/20241128180650_Initial.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlowerShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Role = table.Column<string>(type: "TEXT", nullable: true),
+                    Image = table.Column<string>(type: "TEXT", nullable: true),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false),
+                    City = table.Column<string>(type: "TEXT", nullable: false),
+                    PasswordHash = table.Column<string>(type: "TEXT", nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    Balance = table.Column<double>(type: "REAL", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    ShopFlowers = table.Column<string>(type: "TEXT", nullable: true),
+                    ShopToys = table.Column<string>(type: "TEXT", nullable: true),
+                    ShopPacks = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    Image = table.Column<string>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IEntity_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IEntity_UserId",
+                table: "IEntity",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IEntity");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
